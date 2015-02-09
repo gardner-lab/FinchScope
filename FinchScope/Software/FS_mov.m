9789f55cf671072af252f1cc20195ea5a816543f
WALIII <bliberti@bu.edu>
2015-02-09 18:37:30 -0500

    updated single and dual channel segregation

--- FinchScope/Software/FS_mov.m
+++ FinchScope/Software/FS_mov.m
@@ -4,16 +4,16 @@ function FS_mov(framerate,videodata)
 % Convert FS to avi
 
 % WALIII
-% 12.26.14
-
+% 02.09.15
 
 files = videodata;
-writerObj = VideoWriter( 'Output.avi' );
+writerObj = VideoWriter( 'Output_new','Grayscale AVI' ); % include grayscale AVI if one channel
 writerObj.FrameRate = framerate;
 open(writerObj);
+
 figure;
 for i=1:length(files) %number of images to be read
-    a = files(:,:,:,i);  
+    a = files(:,:,1,i); %1 = just get green channel
     f.cdata = a;
     f.colormap = [];
     writeVideo(writerObj,f);
