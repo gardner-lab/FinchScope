3425aea5e198b75fc0565baed4d18b2427304147
WALIII <bliberti@bu.edu>
2015-11-18 20:38:01 -0500

    edit FS_AV_Parse

--- Analysis Pipeline/FS_AV_Parse.m
+++ Analysis Pipeline/FS_AV_Parse.m
@@ -1,32 +1,28 @@
 
-function FS_AV_Parse2(DIR,day,varargin)
+function FS_AV_Parse(DIR,varargin)
+% FS_AVparse
 
-%========================================%
-%                FS_AVparse              %
-%                 08.02.15               %
-%                  WALIII                %
-%========================================%
-
-
-% day indicates what day in the LS this has occured
+% Parse Data from FreedomScopes
+%   Created: 2015/08/02
+%   By: WALIII
+%   Updated: 2015/11/18
+%   By: WALIII
 
 % FS_AVparse will do several things:
-
+%
 %   1. Seperate Audio and Video, and place them into .mat files in a .mat
 %      directory
 %   2. Make spectrogram .gif files for extracted audio, for perusing
 %      manually.
 %   3. Uses the dependancy extractmedia(), an amazing script created by Nathan Perkins
 
-% Run in the Directory of the .mov files. FS_AVparse should be run first of 
-% the analysis sequence. ** This requires mmreader to function!
+% Run in the Directory of the .mov files. FS_AVparse should be run first:
+% FS_AVparse-->FS_TemplateMatch-->FS_FS_Plot_ROI--> BatchDff2----> FS_Image_ROI--> FS_FS_Plot_ROI
 
-% FS_AVparse-->FS_SongDetect-->FS_DetrendAlign-->FS_ROIselect-->FS_ROIplot
 
 
 mat_dir='mat';
 gif_dir='gif';
-
 
 if exist(mat_dir,'dir') rmdir(mat_dir,'s'); end
 if exist(gif_dir,'dir') rmdir(gif_dir,'s'); end
@@ -57,12 +53,12 @@ for i=1:length(mov_listing)
 
     [path,file,ext]=fileparts(filenames{i});
 
-    
+
 	fprintf(1,formatstring,round((i/length(mov_listing))*100));
     FILE = fullfile(DIR,mov_listing{i})
-    %[video, audio] = mmread(FILE)
-    
-    %Extract Audio and video data...
+
+
+%Extract Audio and video data...
 [a_ts, a, v_ts, v] = extractmedia(FILE);
 
 
@@ -70,7 +66,7 @@ for i=1:length(mov_listing)
 
 % Format VIDEO DATA
 [video.width, video.height, video.channels] = size(v{1});
-video.times = v_ts+0.1703*(day-1);
+video.times = v_ts% 0.1703*(day-1);
 
 video.nrFramesTotal = size(v,2);
 video.FrameRate = 1/mean(diff(v_ts));
@@ -78,6 +74,7 @@ for ii = 1: size(v,2)
     video.frames(ii).cdata(:,:,:) = v{ii};
     video.frames(ii).colormap = [];
 end
+
 % Format AUDIO DATA
 audio.nrChannels = 1;
 audio.bits = 16;
