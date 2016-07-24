7b3500539ba38c48d4512e67d5e90ba91353d0e1
WALIII <bliberti@bu.edu>
2016-07-24 00:08:08 -0400

    add varrargs

--- Analysis Pipeline/FS_tiff.m
+++ Analysis Pipeline/FS_tiff.m
@@ -1,5 +1,5 @@
 
-function FS_tiff(videodata)
+function FS_tiff(videodata,varargin)
   % FS_tiff
 
   %   Create multipage Tiff stack from the .mov files form the FreedomScope
@@ -16,28 +16,48 @@ function FS_tiff(videodata)
   %
   %
 
+  
+
+  
+  nparams=length(varargin);
+
+% if mod(nparams,2)>0
+% 	error('Parameters must be specified as parameter/value pairs');
+% end
+
 [files, n] = FS_Format(videodata,1);
 
-for i = 1: size(files,3)
-    files2(:,:,i) = wiener2(files(:,:,i),[3 3]);
+for i=1:2:nparams
+	switch lower(varargin{i})
+		case 'colors'
+			colors=varargin{i+1};
+            
+        case 's_smooth'
+            a=varargin{i+1};
+			for i = 1: size(files,3)
+                files(:,:,i) = wiener2(files(:,:,i),[a a]);
+            end
+            
+        case 't_smooth'
+			b=varargin{i+1};
+                files = convn(files, single(reshape([1 1 1] / b, 1, 1, [])), 'same');
+            
+	end
 end
+  
 
-%files2 = files;
 
-
-files2 = convn(files2, single(reshape([1 1 1] / 3, 1, 1, [])), 'same');
-
-d = files2;
-files2 = ((d-min(d(:))) ./ (max(d(:)-min(d(:)))))*255;
+% d = files2;
+% files2 = ((d-min(d(:))) ./ (max(d(:)-min(d(:)))))*255;
 
 
 
-imwrite(uint8(files2(:,:,1)),'G.tif');
+imwrite(uint8(files(:,:,1)),'G.tif');
 
 
-for i=2:size(files2,3) %number of images to be read
+for i=2:size(files,3) %number of images to be read
     
-    K = files2(:,:,i);
+    K = files(:,:,i);
     %K = wiener2(files2(:,:,i),[5 5]);
     %G  = filter2(fspecial('average',3),files2(:,:,i));
     %GG = medfilt2(G);
